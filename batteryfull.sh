ea5db7332580369572d907ac0d15f2d2b7feaf89
Dan Ortega <ortega.dan2010@gmail.com>
2022-01-02 23:27:43 -0500

    getting it to fully work in ubuntu 20.04

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
-while true
-do
-    export DISPLAY=:0.0
-    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-    if on_ac_power; then
+#! /bin/bash
+# DISPLAY=:0.0
+while true; do
+    battery_info=$(acpi -b)
+    charging_status=$(echo $battery_info | grep -P -o "\S+harging")
+    battery_percent=$(echo $battery_info | grep -P -o "[0-9]+(?=%)")
+    if [ "$charging_status" = "Charging" ]; then
         if [ "$battery_percent" -gt 95 ]; then
             notify-send -u critical "Battery fully charged"
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
         fi
     fi
-    sleep 300 # (5 minutes)
+    sleep 180 # (3 minutes)
 done
