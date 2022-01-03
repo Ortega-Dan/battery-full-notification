8eeff2c7f8e9b26b2ae12be22c7d10d9920a4ae4
Dan Ortega <ortega.dan2010@gmail.com>
2022-01-03 00:22:45 -0500

    supporting already full status

--- batteryfull.sh
+++ batteryfull.sh
@@ -4,7 +4,9 @@ while true; do
     battery_info=$(acpi -b)
     charging_status=$(echo $battery_info | grep -P -o "\S+harging")
     battery_percent=$(echo $battery_info | grep -P -o "[0-9]+(?=%)")
-    if [ "$charging_status" = "Charging" ]; then
+    # echo $charging_status
+    # echo $battery_percent
+    if [ "$charging_status" == "Charging" ] || [ "$charging_status" == "" ]; then
         if [ "$battery_percent" -gt 96 ]; then
             notify-send -u critical "Battery fully charged"
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
