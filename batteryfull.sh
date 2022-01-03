ee0cbc10ae98613ff39191b774df6a45b3e456b9
Dan Ortega <ortega.dan2010@gmail.com>
2022-01-02 23:39:01 -0500

    adjusting percentage

--- batteryfull.sh
+++ batteryfull.sh
@@ -5,7 +5,7 @@ while true; do
     charging_status=$(echo $battery_info | grep -P -o "\S+harging")
     battery_percent=$(echo $battery_info | grep -P -o "[0-9]+(?=%)")
     if [ "$charging_status" = "Charging" ]; then
-        if [ "$battery_percent" -gt 95 ]; then
+        if [ "$battery_percent" -gt 96 ]; then
             notify-send -u critical "Battery fully charged"
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
