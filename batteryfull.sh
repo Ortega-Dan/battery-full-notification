cac5484c392671f0c0395794a94721b79dfa5708
Dan Ortega <ort13016@byui.edu>
2022-03-06 15:05:36 -0500

    little improvement

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 # DISPLAY=:0.0
 while true; do
-    battery_info=$(acpi -b)
+    battery_info=$(acpi -b|head -n 1)
     charging_status=$(echo $battery_info | grep -P -o "\S+harging")
     battery_percent=$(echo $battery_info | grep -P -o "[0-9]+(?=%)")
     # echo $charging_status
     # echo $battery_percent
-    if [ "$charging_status" == "Charging" ] || [ "$charging_status" == "" ]; then
+    if [ "$charging_status" == "Charging" ] || [ "$charging_status" == "" ] || [ "$charging_status" == "charging" ]
+    then
         if [ "$battery_percent" -gt 96 ]; then
             notify-send -u critical "Battery fully charged"
             paplay /usr/share/sounds/gnome/default/alerts/glass.ogg
